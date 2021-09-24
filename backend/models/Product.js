ca49edbd07b2b35371e921648f5cf5cdb0f59cbf
Vicky Nguyen <ztieu.linh@gmail.com>
2021-09-24 14:52:17 +0700

    update model product

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,71 +1,50 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema(
-  {
-    product_name: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: false,
-    },
-    create_date: {
-      type: Date,
-      default: Date.now,
-    },
-    update_date: {
-      type: Date,
-      default: null,
-    },
-    isHot: {
-      type: Boolean,
-      default: false,
-    },
-    isNew: {
-      type: Boolean,
-      default: false,
-    },
-    status: {
-      type: Boolean,
-      default: true,
-    },
-    view: {
-      type: Number,
-      default: 0,
-    },
-    catalog_id: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: Buffer,
-      required: false,
-    },
-    imageType: {
-      type: String,
-      required: false,
-    },
+const ProductSchema = new mongoose.Schema({
+  product_name: {
+    type: String,
+    required: true,
   },
-  {
-    // the virtual field to be displayed on client side
-    toObject: {
-      virtuals: true,
-    },
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
-ProductSchema.virtual("imagePath").get(function () {
-  if (this.image != null && this.imageType != null) {
-    return `data:${this.imageType};charset=utf-8;base64,${this.image.toString(
-      "base64"
-    )}`;
-  }
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+  },
+  create_date: {
+    type: Date,
+    default: Date.now,
+  },
+  update_date: {
+    type: Date,
+    default: null,
+  },
+  isHot: {
+    type: Boolean,
+    default: false,
+  },
+  isNew: {
+    type: Boolean,
+    default: false,
+  },
+  status: {
+    type: Boolean,
+    default: true,
+  },
+  view: {
+    type: Number,
+    default: 0,
+  },
+  catalog_id: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false,
+  },
 });
+
 module.exports = mongoose.model("Product", ProductSchema);

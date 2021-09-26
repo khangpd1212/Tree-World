145db7f293a349bd48fa7de61519e53f88d44513
Vicky Nguyen <ztieu.linh@gmail.com>
2021-09-27 02:55:01 +0700

    style header

--- frontend/src/components/Layout/BaseLayout.jsx
+++ frontend/src/components/Layout/BaseLayout.jsx
@@ -1,18 +1,20 @@
+import BaseHeader from "components/Base/BaseHeader";
 import { Link, Route } from "react-router-dom";
 import { Home, Users } from "../../pages";
 
 export default function BaseLayout() {
-    return <div className="root-base">
-        <div className="head">
-            <h2>HOME</h2>
-            <Link to={'/'}>home</Link>
-            <Link to={'/admin'}>admin</Link>
-            <Link to={'/users'}>login</Link>
-        </div>
-        <Route path="/" component={Home}/>
-        <Route path="/users" component={Users}/>
-        <div className="footer">
-            Footer
-        </div>
+  return (
+    <div className="root-base">
+      <BaseHeader />
+      <div className="head">
+        <h2>HOME</h2>
+        <Link to={"/"}>home</Link>
+        <Link to={"/admin"}>admin</Link>
+        <Link to={"/users"}>login</Link>
+      </div>
+      <Route path="/" component={Home} />
+      <Route path="/users" component={Users} />
+      <div className="footer">Footer</div>
     </div>
+  );
 }

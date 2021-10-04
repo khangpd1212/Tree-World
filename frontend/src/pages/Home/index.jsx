6d2e41a53b6f75ab4c21839432c645c2bca22722
khangpd1212 <phankd1212@gmail.com>
2021-10-04 18:51:16 +0700

    fix home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -6,20 +6,20 @@ export default function Home(){
     return (
     <>
         <Row className="slider">
-            <Col className="slider-left" xs={24} lg={14}>
+            <Col className="slider-left" xs={24} sm={14}>
                 <img src="./images/slider.png" alt="img_slider-left"/>
             </Col>
-            <Col className="slider-right" lg={10}>
+            <Col className="slider-right" xs={24} sm={10}>
                 <img src="./images/slider2.png" alt="img_slider-right"/>
                 <div className="slider-right_title">
                     <Title>Make your days feeling goods with beautiful plant</Title>
                 </div>
                 <div className="slider-right_content">
                     <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing telit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Paragraph>
-                    <button type="submit" class="btn-home">
-                        See more
-                    </button>
                 </div>
+                <button type="submit" class="btn-home">
+                    See more
+                </button>
             </Col>
         </Row>
     </>

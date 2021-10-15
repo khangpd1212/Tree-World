7adf6443486183c0881c8cf663e6f4c501aaa750
Vicky Nguyen <ztieu.linh@gmail.com>
2021-10-15 20:49:47 +0700

    address

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,15 @@ const productRoute = require("./product");
 const catalogRoute = require("./catalog");
 const authRoute = require("./auth");
 const paymentRoute = require("./payment");
+const addressRoute = require("./address");
 
 const useRouter = (app) => {
-    app
-        .use("/auth", authRoute)
-        .use("/product", productRoute)
-        .use("/catalog", catalogRoute)
-        .use("/payment", paymentRoute)
-}
+  app
+    .use("/auth", authRoute)
+    .use("/product", productRoute)
+    .use("/catalog", catalogRoute)
+    .use("/payment", paymentRoute)
+    .use("/address", addressRoute);
+};
 
-module.exports = { useRouter }
+module.exports = { useRouter };

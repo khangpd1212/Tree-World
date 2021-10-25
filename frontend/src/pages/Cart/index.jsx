3f0c61cf66c0f8237bc6528cc524161ea7389dc1
van <thaovan5501@gmail.com>
2021-10-25 18:49:41 +0700

    finished cart

--- frontend/src/pages/Cart/index.jsx
+++ frontend/src/pages/Cart/index.jsx
@@ -11,6 +11,7 @@ export default function Cart(){
                     <Col xs={10}>
                         <h1>Your Product</h1>
                         <div className="product__left">
+                            <input type="checkbox"/>
                             <img src="/images/product1.png" alt=""/>
                             <h2>Product name</h2>
                         </div>
@@ -22,20 +23,39 @@ export default function Cart(){
                                 <h2>Price</h2>
                                 <h2>Quantity</h2>
                                 <h2>Total</h2>
+                                <h2>Action</h2>
                             </div>
                             <div className="product__right--content">
-                                    <p>Blue</p>
+                                    <p className="product__color">Blue</p>
                                     <p>$28.00</p>
                                     <div className="btn-sl">
                                         <input type="number" value="1"/>
                                     </div>
-                                    <p>$28.00</p>
+                                    <p className="product__total">$28.00</p>
+                                    <button className="cart__button">X</button>
                             </div>
-                            
                         </div>
                     </Col>
                 </Row>
             </div>
+            <div className="cart__total">
+                <Row className="cart__total--bottom">
+                    <Col xs={20}>
+                        <div className="total--left">
+                            <input type="checkbox" />
+                            <span>Select All Products</span>
+                        </div>
+                            
+                    </Col>
+                    <Col xs={4}>
+                        <div className="total--right">
+                            <span className="total__all">Total: <span className="price">$28</span></span>
+                            <button className="cart__button">Buy now</button>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+            
         </div>
     )
 }

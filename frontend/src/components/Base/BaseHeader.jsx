9a9026db7abbf7d91ee21fd657816af443a0edff
Vicky Nguyen <ztieu.linhz@gmail.com>
2021-10-31 20:53:29 +0700

    update

--- frontend/src/components/Base/BaseHeader.jsx
+++ frontend/src/components/Base/BaseHeader.jsx
@@ -1,22 +1,15 @@
 import { MenuOutlined } from "@ant-design/icons";
-import {
-  Layout,
-  Anchor,
-  Button,
-  Drawer,
-  Row,
-  Col,
-  Modal,
-  Checkbox,
-} from "antd";
+import { Layout, Anchor, Button, Drawer, Row, Col, Modal, Menu } from "antd";
 import React, { useEffect, useState } from "react";
 import { Link as LinkRoute } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCatalogs } from "redux/catalog";
 import "styles/header.scss";
 import "styles/login.scss";
+
 function BaseHeader() {
   const { Header } = Layout;
+  const { SubMenu } = Menu;
   const [visible, setVisible] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [show, handleShow] = useState(false);
@@ -88,7 +81,9 @@ function BaseHeader() {
                       catalogList.map((item, index) => (
                         <Col span={12} key={index}>
                           <li>
-                            <LinkRoute>{item.catalog_name}</LinkRoute>
+                            <LinkRoute to={`/product/${item.catalog_name}`}>
+                              {item.catalog_name}
+                            </LinkRoute>
                           </li>
                         </Col>
                       ))}
@@ -124,28 +119,50 @@ function BaseHeader() {
             >
               <Anchor>
                 <div className="navbar__link">
-                  <LinkRoute to={"/"} onClick={onClose}>
-                    Home
-                  </LinkRoute>
-                  <LinkRoute to={"/"} onClick={onClose}>
-                    About Us
-                  </LinkRoute>
-                  <LinkRoute to={"/product"} onClick={onClose}>
-                    Product
-                  </LinkRoute>
-                  <LinkRoute to={"/"} onClick={onClose}>
-                    Blog
-                  </LinkRoute>
-                  <LinkRoute to={"/contact"} onClick={onClose}>
-                    Contact Us
-                  </LinkRoute>
-                  <LinkRoute to={"/cart"} onClick={onClose}>
-                    Cart
-                  </LinkRoute>
-                  <LinkRoute to={"/login"} onClick={onClose}>
-                    Login
-                  </LinkRoute>
-                  <LinkRoute to={"/admin"}>Admin</LinkRoute>
+                  <div className="navbar__menu">
+                    <LinkRoute to={"/"} onClick={onClose}>
+                      Home
+                    </LinkRoute>
+                  </div>
+                  <div className="navbar__menu">
+                    <LinkRoute to={"/"} onClick={onClose}>
+                      About Us
+                    </LinkRoute>
+                  </div>
+                  <div className="navbar__menu dropdown__menu">
+                    <LinkRoute to={"/product"} onClick={onClose}>
+                      Product
+                    </LinkRoute>
+                    <Menu mode="inline">
+                      <SubMenu>
+                        <Menu.Item>Cactus</Menu.Item>
+                      </SubMenu>
+                    </Menu>
+                  </div>
+                  <div className="navbar__menu">
+                    <LinkRoute to={"/"} onClick={onClose}>
+                      Blog
+                    </LinkRoute>
+                  </div>
+                  <div className="navbar__menu">
+                    <LinkRoute to={"/contact"} onClick={onClose}>
+                      Contact Us
+                    </LinkRoute>
+                  </div>
+                  <div className="navbar__menu">
+                    {" "}
+                    <LinkRoute to={"/cart"} onClick={onClose}>
+                      Cart
+                    </LinkRoute>
+                  </div>
+                  <div className="navbar__menu">
+                    <LinkRoute to={"/login"} onClick={onClose}>
+                      Login
+                    </LinkRoute>
+                  </div>
+                  <div className="navbar__menu">
+                    <LinkRoute to={"/admin"}>Admin</LinkRoute>
+                  </div>
                 </div>
               </Anchor>
             </Drawer>

b37d194e4929fda292042a2cafcf51077586dc8d
Vicky Nguyen <ztieu.linhz@gmail.com>
2021-11-05 12:26:37 +0700

    hihi

--- frontend/src/components/Layout/AdminLayout.jsx
+++ frontend/src/components/Layout/AdminLayout.jsx
@@ -1,26 +1,28 @@
-import { Layout } from 'antd';
+import { Layout } from "antd";
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import "styles/admin.scss";
 import HeaderAdmin from "../Admin/HeaderAdmin";
 import SideComponent from "../Admin/SideComponent";
-import {ProductAdmin, CategoryAdmin, AddProduct} from "../../pages";
+import { ProductAdmin, CategoryAdmin, AddProduct } from "../../pages";
 const { Content } = Layout;
 
 export default function AdminLayout() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    return <div className="root-admin">
-        <Layout>
-            <SideComponent isOpen={isOpen} />
-            <Layout className="site-layout">
-                <HeaderAdmin isOpen={isOpen} setIsOpen={setIsOpen} />
-                <Content>
-                    <Route path="/admin/product" exact component={ProductAdmin} />
-                    <Route path="/admin/product/add" exact component={AddProduct} />
-                    <Route path="/admin/category" exact component={CategoryAdmin} />
-                </Content>
-            </Layout>
+  return (
+    <div className="root-admin">
+      <Layout>
+        <SideComponent isOpen={isOpen} />
+        <Layout className="site-layout">
+          <HeaderAdmin isOpen={isOpen} setIsOpen={setIsOpen} />
+          <Content>
+            <Route path="/admin/product" exact component={ProductAdmin} />
+            <Route path="/admin/product/add" exact component={AddProduct} />
+            <Route path="/admin/category" exact component={CategoryAdmin} />
+          </Content>
         </Layout>
+      </Layout>
     </div>
+  );
 }

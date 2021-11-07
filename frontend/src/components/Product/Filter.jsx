9005e0c38a05735ed0a5d53ae46243a1323e27dc
Vicky Nguyen <ztieu.linhz@gmail.com>
2021-11-07 23:43:39 +0700

    filter price

--- frontend/src/components/Product/Filter.jsx
+++ frontend/src/components/Product/Filter.jsx
@@ -1,8 +1,30 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Button, Radio, Row, Select } from "antd";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+import { setUrlStatus } from "redux/layout";
 const { Option } = Select;
 function Filter() {
+  const history = useHistory();
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const changePrice = (value) => {
+    console.log(location);
+    // dispatch(
+    //   setUrlStatus({
+    //     pathname: `${location.pathname}?order=${value}&price=true`,
+    //     search: `?order=${value}&price=true`,
+    //   })
+    // );
+    history.push({
+      pathname: location.pathname,
+      search: `?order=${value}&price=true`,
+    });
+  };
+  const changeSort = (value) => {
+    console.log(value);
+  };
   return (
     <div className="product__section--filter">
       <Row justify="space-between" align="middle">
@@ -12,15 +34,20 @@ function Filter() {
           </Select>
         </div>
         <div>
-          <Radio.Group defaultValue="default" buttonStyle="solid">
+          <Radio.Group
+            defaultValue="default"
+            buttonStyle="solid"
+            onChange={changeSort}
+          >
             <Radio.Button value="default">Default Sorting</Radio.Button>
             <Radio.Button value="hot">Hot New</Radio.Button>
             <Radio.Button value="best">Best Seller</Radio.Button>
+
+            <Select placeholder="PRICE" onChange={changePrice}>
+              <Option value="asc">Low to High</Option>
+              <Option value="desc">High to Low</Option>
+            </Select>
           </Radio.Group>
-          <Select placeholder="PRICE">
-            <Option value="asc">Low to High</Option>
-            <Option value="desc">High to Low</Option>
-          </Select>
         </div>
         <div className="pagination__btn">
           <span className="pagination__btn--show">1/100</span>

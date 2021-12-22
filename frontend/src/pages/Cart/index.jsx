03eb6612b1f79bdadee3e009d069e990f144d356
thovannn <phankd1212@gmail.com>
2021-12-23 06:35:42 +0700

    fix header

--- frontend/src/pages/Cart/index.jsx
+++ frontend/src/pages/Cart/index.jsx
@@ -3,12 +3,31 @@ import CartItem from "components/Cart/CartItem";
 import CartTotal from "components/Cart/CartTotal";
 import BreadCrumb from "components/Base/BreadCrumb";
 import SliderProduct from "components/Home/SliderProduct";
-import { useSelector } from "react-redux";
 import { selectProducts } from "redux/product";
+import { selectUsers } from "redux/user";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { ShowModalLogin } from "redux/modal";
 
 export default function Cart() {
+  const dispatch = useDispatch();
+
   const { productList } = useSelector(selectProducts);
   const showProduct = productList.filter((item) => item.status === true);
+  const { userItems } = useSelector(selectUsers);
+
+  const tokenLocal = localStorage.getItem("token");
+  let token = tokenLocal ? JSON.parse(tokenLocal) : userItems.accessToken;
+  if (!token) {
+    dispatch(ShowModalLogin(true));
+    return (
+      <Redirect
+        to={{
+          pathname: document.referrer,
+        }}
+      />
+    );
+  }
   return (
     <div id="cart">
       <BreadCrumb className="breadcrumb" page="Cart" />
